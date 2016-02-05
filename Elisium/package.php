c247e3977beaae1b044a83cd6d5b4e184c245899
kayila <arekuanubis@gmail.com>
2016-02-05 16:19:05 -0500

    Create package.php
    Added Elisium/package.php to allow jenkins to auto-create the correct packages.php section.

---
+++ Elisium/package.php
@@ -0,0 +1,11 @@
+<?php
+
+$packages[] = array(
+    'name' => 'Elisium',
+    'title' => 'Elisium',
+    'version' => 'VERSION',
+    'priority' => 0,
+    'location' => 'modpacks/elisium.json',
+);
+
+?>
